905acab95df1a94d0e14dcf83ec383fd98fa177b
crb <cbanger@sl.on.ca>
2021-10-28 08:44:10 -0400

    add mongo queries module

---
+++ mongo/queries.js
@@ -0,0 +1,22 @@
+const mongoClient = require("./config");
+
+async function findListing(response)
+{
+  console.log('Find Listing');
+  let connection = await mongoClient.connect()
+  console.log('Connected');
+  let db = await connection.db('sample_airbnb');
+  console.log('open airbnb databse')
+
+  let listingsAndReviews = await db.collections('ListingsAndReviews')
+  console.log('select listings and reviews')
+
+  let listing = await listingsAndReviews.findOne({})
+
+  console.log('Close db')
+
+  response.send(listing)
+  connection.close()  
+}
+
+module.exports = {findListing}
--- routes/airbnb.js
+++ routes/airbnb.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var mongoClient = require('./../mongo/config')
+var mongoQueries = require('./../mongo/queries')
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res) => {
   res.render('airbnb', {title:'AirBnb', mongoHost:mongoClient.options.srvHost});
 });
+
+
+router.get('/find1', (req,res)=>{
+  mongoQueries.findListing(res);
+})
+
+
+
+
 
 module.exports = router;
